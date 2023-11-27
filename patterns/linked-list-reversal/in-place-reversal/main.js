b07427c86f78abc1d6228112e62523b1591d172c
Anindya Dey <andy.ani1992@gmail.com>
2023-11-27 23:11:32 +0530

    add in-place reversal of linked list

---
+++ patterns/linked-list-reversal/in-place-reversal/main.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (!head) return head;
+
+    let prev = null;
+    let curr = head;
+
+    while (curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+};
